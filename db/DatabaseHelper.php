f8b7b45e1aaaa30ca5bfb8f358da3c566bc08852
Francesco Valentini <fv26012001@gmail.com>
2022-12-25 03:47:42 +0100

    Make comments better

--- db/DatabaseHelper.php
+++ db/DatabaseHelper.php
@@ -7,17 +7,22 @@ class DatabaseHelper
     {
         $this->db = new mysqli($servername, $username, $password, $dbname, $port);
         if ($this->db->connect_error) {
-            die("Connection failed: " . $db->connect_error);
+            die("Connection failed: " . $this->db->connect_error);
         }
     }
 
     /** Adds a user to DB.
+     * @param $id user id (username)
+     * @param $name user name (his normal name, not his username)
+     * @param $email account email
+     * @param $password account password
+     * @param $picture_path path to user picture (saved outside DB)
      * @return true if user has been added
      * @return false if user could not be added - a user with the same ID already exists
      */
     public function addUser($id, $name, $email, $password, $picture_path)
     {
-        if (checkUserIDAvailability($id)) {
+        if ($this->checkUserIDAvailability($id)) {
             $query = "INSERT INTO users (user_id, user_name, user_email, user_password, user_picture_path, user_bio, num_posts, num_followers, num_following) 
             VALUES (?, ?, ?, ?, ?, NULL, 0, 0, 0, 0)";
             $stmt = $this->db->prepare($query);
