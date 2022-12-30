e4ca823f66403e776f8f6bdfd324835918cffc1d
Francesco Valentini <fv26012001@gmail.com>
2022-12-30 03:10:44 +0100

    Add createPost()

--- db/CreateTrait.php
+++ db/CreateTrait.php
@@ -23,11 +23,37 @@ trait CreateTrait
     }
 
     /**
+     * Add a post to DB.
+     * @param string $content text of post
+     * @param string $user user id
+     * @param array $media_path_array list of media paths (if any)
+     */
+    public function createPost($content, $user, $media_path_array = null)
+    {
+        $size = count($media_path_array);
+        if ($size <= 10) {
+            // fill array with null values if there are less than 10 image paths
+            for ($i = 0; $i < 10; $i++) {
+                if ($i >= $size) {
+                    $media_path_array[$i] = null;
+                }
+            }
+            $query = "INSERT INTO posts (post_id, content, media_path0, media_path1, media_path2, media_path3, media_path4, media_path5, media_path6, media_path7, media_path8, media_path9, post_time, num_likes, num_comments, num_tags, user_id) 
+                  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW(), 0, 0, 0, ?)";
+            $stmt = $this->db->prepare($query);
+            $stmt->bind_param("sssssssssssss", $this->getNextPostID(), $content, 
+                    $media_path_array[0], $media_path_array[1], $media_path_array[2], 
+                    $media_path_array[3], $media_path_array[4], $media_path_array[5], 
+                    $media_path_array[6], $media_path_array[7], $media_path_array[8], 
+                    $media_path_array[9], $user);
+        }
+    }
+
+    /**
      * Add a comment to DB.
      * @param mixed $content text of comment
-     * @param mixed $user
-     * @param mixed $post
-     * @return void
+     * @param mixed $user user id
+     * @param mixed $post post id
      */
     public function createComment($content, $user, $post)
     {
