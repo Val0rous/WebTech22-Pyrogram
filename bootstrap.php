5d93946faf89b98c177ce377484a02651254c09e
Francesco Valentini <fv26012001@gmail.com>
2022-12-31 01:06:38 +0100

    Change UI: HTML & CSS

--- bootstrap.php
+++ bootstrap.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 require_once "home.php";
+require_once "db/DatabaseHelper.php";
 //define("UPLOAD_DIR", "./upload/");
 //require_once("utils/functions.php");
 ////require_once("db/database.php");
 $dbh = new DatabaseHelper();
-
--- templates/base.php
+++ templates/base.php
@@ -3,14 +3,19 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title></title>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
     <link rel="stylesheet" type="text/css" href="../css/navbar_style.css" />
 </head>
 <body>
     <!-- Top bar header -->
     <header>
-        <h1>Pyrogram</h1>
-        <input id="#messages" class="navbar-icon" type="image" src="../img/messages.png" alt="Messages">  <!-- Andrà modificato poi aggiungendo il collegamento alla pagina -->
+        <div class="logo">
+            <a class="no-underline" href="home.php">
+                Pyrogram
+            </a>
+        </div>
+        <a class="messages-icon no-underline" href="messages.php">
+            <img class="navbar-icon" src="../img/messages.png" alt="Messages">
+        </a>
     </header>
 
     <!-- Content to be added with PHP later -->
@@ -27,25 +32,21 @@
 
     <!-- Navigation bar -->
     <nav>
-            <a href="home.php"><img class="navbar-icon" src="../img/home.png" alt="Home"/></a>
-            <a href="search.php"><img class="navbar-icon" src="../img/search.png" alt="Search"/></a>
-            <a href="create.php"><img class="navbar-icon" src="../img/create.png" alt="Create"/></a>
-            <a href="notifications.php"><img class="navbar-icon" src="../img/notifications.png" alt="Notifications"/></a>
-            <a href="profile.php"><img class="navbar-icon" src="../img/profile.png" alt="Profile"/></a>
-            
-            <!--
-            <li><input type="image" src="home.png" alt="Home"></li>
-            <li><input type="image" src="search.png" alt="Search"></li>
-            <li><input type="image" src="create.png" alt="Create"></li>
-            <li><input type="image" src="notifications.png" alt="Notifications"></li>
-            <li><input type="image" src="profile.png" alt="Profile"></li>
-
-            <li></li><a href="home.html">Home</a></li>
-            <li><a href="search.html">Search</a></li>
-            <li><a href="create.html">Create</a></li>
-            <li><a href="notifications.html">Notifications</a></li>
-            <li><a href="profile.html">Profile</a></li>
-            -->
+            <a class="home-icon no-underline" href="home.php">
+                <img class="navbar-icon" src="../img/home.png" alt="Home"/>
+            </a>
+            <a class="search-icon no-underline" href="search.php">
+                <img class="navbar-icon" src="../img/search.png" alt="Search"/>
+            </a>
+            <a class="create-icon no-underline" href="create.php">
+                <img class="navbar-icon" src="../img/create.png" alt="Create"/>
+            </a>
+            <a class="notifications-icon no-underline" href="notifications.php">
+                <img class="navbar-icon" src="../img/notifications.png" alt="Notifications"/>
+            </a>
+            <a class="profile-icon no-underline" href="profile.php">
+                <img class="navbar-icon" src="../img/profile.png" alt="Profile"/>
+            </a>
     </nav>
 </body>
 </html>
