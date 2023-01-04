bfa3723ce9020e42dd5b311cb8c7178817353286
Francesco Valentini <fv26012001@gmail.com>
2023-01-04 02:43:08 +0100

    Create queries

--- db/CreateTrait.php
+++ db/CreateTrait.php
@@ -43,11 +43,12 @@ trait CreateTrait
             $stmt = $this->db->prepare($query);
             $next_post_id = $this->getNextPostID();
             $stmt->bind_param("sssssssssssss", $next_post_id, $content,
-                    $media_path_array[0], $media_path_array[1], $media_path_array[2], 
-                    $media_path_array[3], $media_path_array[4], $media_path_array[5], 
-                    $media_path_array[6], $media_path_array[7], $media_path_array[8], 
+                    $media_path_array[0], $media_path_array[1], $media_path_array[2],
+                    $media_path_array[3], $media_path_array[4], $media_path_array[5],
+                    $media_path_array[6], $media_path_array[7], $media_path_array[8],
                     $media_path_array[9], $user);
             $stmt->execute();
+            $this->incNumPosts($user);
         }
     }
 
@@ -65,5 +66,22 @@ trait CreateTrait
         $next_comment_id = $this->getNextCommentID();
         $stmt->bind_param("ssss", $next_comment_id, $content, $user, $post);
         $stmt->execute();
+    }
+
+    /**
+     * Add a following to DB.
+     * @param $user_following user who follows another one
+     * @param $user_followed user who is followed by another one
+     * @return void
+     */
+    public function createFollowing($user_following, $user_followed)
+    {
+        $query = "INSERT INTO followings (user_id_following, user_id_followed) 
+                  VALUES (?, ?)";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("ss", $user_following, $user_followed);
+        $stmt->execute();
+        $this->incNumFollowing($user_following);
+        $this->incNumFollowers($user_followed);
     }
 }
--- db/DeleteTrait.php
+++ db/DeleteTrait.php
@@ -51,4 +51,38 @@ trait DeleteTrait
         $stmt->bind_param("s", $user);
         $stmt->execute();
     }
+
+    /**
+     * Delete a post from DB.
+     * @param $post post id
+     * @return void
+     */
+    public function deletePost($post)
+    {
+        $user = $this->findPost($post)["user_id"];
+        $query = "DELETE FROM posts 
+                  WHERE post_id = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("s", $post);
+        $stmt->execute();
+        $this->decNumPosts($user);
+    }
+
+    /**
+     * Delete a following from DB.
+     * @param $user_following user who follows another one
+     * @param $user_followed user who is followed by another one
+     * @return void
+     */
+    public function deleteFollowing($user_following, $user_followed)
+    {
+        $query = "DELETE FROM followings 
+                  WHERE user_id_following = ? 
+                  AND user_id_followed = ?";
+        $stmt = $this->db->prepare($query);
+        $stmt->bind_param("ss", $user_following, $user_followed);
+        $stmt->execute();
+        $this->decNumFollowing($user_following);
+        $this->decNumFollowers($user_followed);
+    }
 }
