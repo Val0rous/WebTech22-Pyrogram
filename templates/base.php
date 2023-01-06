3f80f7d1cfa6a5fbd2f6f5226b973dd8932a846e
Francesco Valentini <fv26012001@gmail.com>
2023-01-06 01:19:49 +0100

    Make transition from CSS to SCSS

--- templates/base.php
+++ templates/base.php
@@ -3,7 +3,7 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title></title>
-    <link rel="stylesheet" type="text/css" href="../css/navbar_style.css" />
+    <link rel="stylesheet" type="text/css" href="../scss/navbar.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script type="text/javascript" src="../js/scroll_animation.js"></script>
 </head>
