bcdf4154cbcf150aff5f89eea11500c9ea5fdb3f
Francesco Valentini <fv26012001@gmail.com>
2023-01-15 04:53:49 +0100

    UX improvements

--- templates/signup.php
+++ templates/signup.php
@@ -20,23 +20,46 @@
             <input type="text" id="email" name="email" placeholder=" ">
             <label for="email">Email</label>
         </div>
+        <div class="warning">
+            <span>
+                <svg class="invalid" viewBox="0 0 24 24">
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z">
+                    </path>
+                </svg>
+            </span>
+            Invalid email format
+        </div>
         <div class="input-box">
             <input type="text" id="username" name="username" placeholder=" ">
             <label for="username">Username</label>
+        </div>
+        <div class="warning">
+            <span>
+                <svg class="invalid" viewBox="0 0 24 24">
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z">
+                    </path>
+                </svg>
+            </span>
+            Username can only contain letters, numbers, period and underscore
         </div>
         <div class="input-box">
             <input type="password" id="password" name="password" placeholder=" ">
             <label for="password">Password</label>
             <button type="button" class="show-password">Show</button>
         </div>
+        <div class="warning">
+            <span>
+                <svg class="invalid" viewBox="0 0 24 24">
+                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z">
+                    </path>
+                </svg>
+            </span>
+            Password must be at least 8 characters long and include both uppercase and lowercase letters, numbers, and symbols
+        </div>
         <button type="submit" id="submit-button" name="signup" value="signup" disabled>Sign Up</button>
     </form>
     <div class="container">
         Already have an account?&nbsp;<a href="login.php">Log In</a>
     </div>
-        <svg class="invalid">
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z">
-            </path>
-        </svg>
 </main>
 </html>
