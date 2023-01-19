51d2702a162c411e2616ce0fa7e4d35425b2f0d5
Francesco Valentini <fv26012001@gmail.com>
2023-01-19 01:48:59 +0100

    Fix default profile image location

--- php/api_signup.php
+++ php/api_signup.php
@@ -6,7 +6,7 @@ if (isset($_POST["email"]) and
     isset($_POST["username"]) and
     isset($_POST["password"])) {
     $db = new DatabaseHelper();
-    if ($db->createUser($_POST["username"], $_POST["full_name"], $_POST["email"], $_POST["password"], "db/media/profile_pics/default.png")) {
+    if ($db->createUser($_POST["username"], $_POST["full_name"], $_POST["email"], $_POST["password"], "img/default_profile_pic.jpg")) {  //"db/media/profile_pics/default.png"
         echo json_encode(array("success" => 1));
     } else {
         echo json_encode(array("success" => 0));
