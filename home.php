508dc7a1d9a5d7abdcec5c5beb48c7a71a087bfa
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2023-01-20 12:10:36 +0100

    Change forgotten var name

--- home.php
+++ home.php
@@ -14,8 +14,8 @@ $_SESSION["user"]["user_name"] = "user_name";
 // to erase
 $text = "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-//$templateParams["posts"] = $dbh->getRandomPosts(5, $templateParams["userID"]);        // test example
-$templateParams["posts"] = array(array("user_image"=>"img/default_profile_pic.jpg",   // example
+//$templateParams["posts"] = $dbh->getRandomPosts(5, $_SESSION["user"]["user_id"]);     // test example
+$templateParams["posts"] = array(array("user_image"=>"img/default_profile_pic.jpg",     // example
         "user_name"=>"Name", "date"=>"07/01/2023", "content"=>$text, "media"=>array("img/08.png"),
         "num_likes"=>10, "num_comments"=>5),
         array("user_image"=>"img/default_profile_pic.jpg",
