53083ac4a6c2a4281e50ded4a0f1d8a912870208
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2023-01-20 14:56:45 +0100

    Update notifications page (html and css)

--- notifications.php
+++ notifications.php
@@ -4,5 +4,19 @@ require_once "bootstrap.php";
 // Params for the Base Template
 $templateParams["name"] = "notifications.php";
 $templateParams["css"] = "notifications.css";
+// notification type images
+$templateParams["notification_type"]["like"]["img"] = "image-icon.jpg";
+$templateParams["notification_type"]["like"]["alt"] = "post";
+$templateParams["notification_type"]["comment"]["img"] = "image-icon.jpg";
+$templateParams["notification_type"]["comment"]["alt"] = "post";
+$templateParams["notification_type"]["follow"]["img"] = "";         // ?
+$templateParams["notification_type"]["follow"]["alt"] = "follow";   // ?
+
+//$templateParams["notifications"] = $dbh->getUserNotifications($_SESSION["user"]["user_id"]);  // test example
+$templateParams["notifications"] = array(array("user_image"=>"img/default_profile_pic.jpg",     // example
+        "user_name"=>"Name", "content"=>"liked your post.", "type"=>"like"),
+        array("user_image"=>"img/default_profile_pic.jpg",
+        "user_name"=>"Name2", "content"=>"commented your post.", "type"=>"comment")
+);
 
 require "templates/base.php";
--- templates/notifications.php
+++ templates/notifications.php
@@ -1,2 +1,12 @@
 <!-- In main -->
 <h1>Notifications</h1>
+<section>
+    <?php foreach ($templateParams["notifications"] as $notification): ?>
+    <section>
+        <img src="<?=$notification["user_image"]?>" alt="<?=$post["user_name"]?>'s user image"/>
+        <p><strong><?=$notification["user_name"]?></strong> <?=$notification["content"]?></p>
+        <img src="img/<?=$templateParams["notification_type"][$notification["type"]]["img"]?>"
+             alt="<?=$templateParams["notification_type"][$notification["type"]]["alt"]?>"/>
+    </section>
+    <?php endforeach; ?>
+</section>
