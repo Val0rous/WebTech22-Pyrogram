b03ffa78262b0a1ec4f464681d7ed0ff7221960e
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2023-01-20 16:12:09 +0100

    Add follow/following to the notifications page

--- notifications.php
+++ notifications.php
@@ -9,14 +9,18 @@ $templateParams["notification_type"]["like"]["img"] = "image-icon.jpg";
 $templateParams["notification_type"]["like"]["alt"] = "post";
 $templateParams["notification_type"]["comment"]["img"] = "image-icon.jpg";
 $templateParams["notification_type"]["comment"]["alt"] = "post";
-$templateParams["notification_type"]["follow"]["img"] = "";         // ?
-$templateParams["notification_type"]["follow"]["alt"] = "follow";   // ?
+/*$templateParams["notification_type"]["follow"]["img"]["yes"] = "";          // ?
+$templateParams["notification_type"]["follow"]["alt"]["yes"] = "following"; // ?
+$templateParams["notification_type"]["follow"]["img"]["no"] = "";           // ?
+$templateParams["notification_type"]["follow"]["alt"]["no"] = "follow";     // ?*/
 
 //$templateParams["notifications"] = $dbh->getUserNotifications($_SESSION["user"]["user_id"]);  // test example
 $templateParams["notifications"] = array(array("user_image"=>"img/default_profile_pic.jpg",     // example
-        "user_name"=>"Name", "content"=>"liked your post.", "type"=>"like"),
+        "user_name"=>"Name", "content"=>"liked your post.", "type"=>"like", "user_id"=>""),
         array("user_image"=>"img/default_profile_pic.jpg",
-        "user_name"=>"Name2", "content"=>"commented your post.", "type"=>"comment")
+        "user_name"=>"Name2", "content"=>"commented your post.", "type"=>"comment", "user_id"=>""),
+        array("user_image"=>"img/default_profile_pic.jpg",
+        "user_name"=>"Name3", "content"=>"started following you.", "type"=>"follow", "user_id"=>"")
 );
 
 require "templates/base.php";
--- templates/notifications.php
+++ templates/notifications.php
@@ -5,8 +5,16 @@
     <section>
         <img src="<?=$notification["user_image"]?>" alt="<?=$post["user_name"]?>'s user image"/>
         <p><strong><?=$notification["user_name"]?></strong> <?=$notification["content"]?></p>
-        <img src="img/<?=$templateParams["notification_type"][$notification["type"]]["img"]?>"
-             alt="<?=$templateParams["notification_type"][$notification["type"]]["alt"]?>"/>
+        <?php if ($notification["type"] == "follow"): ?>
+            <?php //if ($dbh->isFollowing($_SESSION["user"]["user_id"], $notification["user_id"])): ?>
+                <!--<input type="button" value="Following" disabled/> -->
+            <?php //else: ?>
+                <input type="button" value="Follow"/>
+            <?php //endif; ?>
+        <?php else: ?>
+            <img src="img/<?=$templateParams["notification_type"][$notification["type"]]["img"]?>"
+                 alt="<?=$templateParams["notification_type"][$notification["type"]]["alt"]?>"/>
+        <?php endif; ?>
     </section>
     <?php endforeach; ?>
 </section>
