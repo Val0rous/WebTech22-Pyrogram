738e2bda8a4126d657d273c186cdb6c215d4797c
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2023-01-22 19:16:14 +0100

    Update notifications page style

--- notifications.php
+++ notifications.php
@@ -5,24 +5,6 @@ require_once "bootstrap.php";
 $templateParams["name"] = "notifications.php";
 $templateParams["css"] = "notifications.css";
 
-/*
-// notification type images
-$templateParams["notification_type"]["like"]["img"] = "image-icon.jpg";
-$templateParams["notification_type"]["like"]["alt"] = "post";
-$templateParams["notification_type"]["comment"]["img"] = "image-icon.jpg";
-$templateParams["notification_type"]["comment"]["alt"] = "post";
-
-//pasting it here from templates/notifications.php, in <?php else: ?>
-<img src="img/<?=$templateParams["notification_type"][$notification["type"]]["img"]?>"
-                     alt="<?=$templateParams["notification_type"][$notification["type"]]["alt"]?>"/>
-*/
-
-/*$templateParams["notification_type"]["follow"]["img"]["yes"] = "";          // ?
-$templateParams["notification_type"]["follow"]["alt"]["yes"] = "following"; // ?
-$templateParams["notification_type"]["follow"]["img"]["no"] = "";           // ?
-$templateParams["notification_type"]["follow"]["alt"]["no"] = "follow";     // ?*/
-
-//$templateParams["notifications"] = $dbh->getUserNotifications($_SESSION["user"]["user_id"]);  // test example
 $db = new DatabaseHelper();
 $templateParams["notifications"] = $db->fetchAllNotifications($_SESSION["user"]["user_id"]);
 /*
@@ -42,4 +24,5 @@ array_push($templateParams["notifications"],    //example
         "type" => "follow", "user_id" => "")
 );
 */
+
 require "templates/base.php";
