1614a3237e0798eb3fd453b35dd27551c54457e2
Francesco Valentini <fv26012001@gmail.com>
2023-01-23 02:32:43 +0100

    Improve notifications layout, simplify HTML by removing a useless tag

--- templates/notifications.php
+++ templates/notifications.php
@@ -5,48 +5,46 @@
     <?php $db = new DatabaseHelper(); ?>
     <?php $sender = $db->findUser($notification["sender_id"]); ?>
     <!-- Notification -->
-    <section>
+    <?php switch ($notification["notification_type"]):
+        case "f": ?>
+            <a href="user_profile.php" class="no-underline">
+        <?php break;
+        case "c":
+        case "l":
+        case "p":
+        case "t": ?>
+            <a href="view_post.php" class="no-underline">
+        <?php break;
+        case "s": ?>
+            <a href="view_story.php" class="no-underline">
+        <?php break;
+        default: ?>
+            <a href="#" class="no-underline">
+    <?php endswitch; ?>
+
+    <!-- Sender profile picture -->
+    <img src="<?=$sender["user_picture_path"]?>" alt="<?=$sender["user_id"]?>'s user image">
+    <!-- Sender username + notification content -->
+    <p><strong><?=$sender["user_id"]?></strong> <?=$notification["content"]?></p>
+
         <?php switch ($notification["notification_type"]):
-            case "f": ?>
-                <a href="user_profile.php" class="no-underline">
-            <?php break;
+            case "f":
+                if ($db->findFollowing($_SESSION["user"]["user_id"], $notification["sender_id"])): ?>
+                    <button type="button" value="following">Following</button>
+                <?php else: ?>
+                    <button type="button" value="follow">Follow</button>
+                <?php endif;
+            break;
             case "c":
             case "l":
             case "p":
             case "t": ?>
-                <a href="view_post.php" class="no-underline">
-            <?php break;
-            case "s": ?>
-                <a href="view_story.php" class="no-underline">
+                <!-- Only takes the first image as thumbnail: this is not a bug -->
+                <img src="<?=$db->findPost($notification["post_id"])["media_path0"]?>"
+                     alt="<?=$sender["user_id"]?>'s post">
             <?php break;
             default: ?>
-                <a href="#" class="no-underline">
         <?php endswitch; ?>
-
-        <!-- Sender profile picture -->
-        <img src="<?=$sender["user_picture_path"]?>" alt="<?=$sender["user_id"]?>'s user image">
-        <!-- Sender username + notification content -->
-        <p><strong><?=$sender["user_id"]?></strong> <?=$notification["content"]?></p>
-
-            <?php switch ($notification["notification_type"]):
-                case "f":
-                    if ($db->findFollowing($_SESSION["user"]["user_id"], $notification["sender_id"])): ?>
-                        <input type="button" value="Following">
-                    <?php else: ?>
-                        <input type="button" value="Follow">
-                    <?php endif;
-                break;
-                case "c":
-                case "l":
-                case "p":
-                case "t": ?>
-                    <!-- Only takes the first image as thumbnail: this is not a bug -->
-                    <img src="<?=$db->findPost($notification["post_id"])["media_path0"]?>"
-                         alt="<?=$sender["user_id"]?>'s post">
-                <?php break;
-                default: ?>
-            <?php endswitch; ?>
-        </a>
-    </section>
+    </a>
     <?php endforeach; ?>
 </section>
