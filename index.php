2dff36611368a05977fd3e78e603283a433f62e9
Francesco Valentini <fv26012001@gmail.com>
2023-01-24 22:43:06 +0100

    Fix cookies loading user on startup

--- index.php
+++ index.php
@@ -3,7 +3,8 @@ require_once "bootstrap.php";
 
 if (isset($_COOKIE["user"]) and
     isset($_COOKIE["token"])) {
-    $_SESSION["user"] = $_COOKIE["user"];
+    $db = new DatabaseHelper();
+    $_SESSION["user"] = $db->findUser($_COOKIE["user"]);
     //require "home.php";
     header("Location: home.php", TRUE, 301);
 } else {
