07c63c88e84d4f8e5522af9bd6a815b1a08844cd
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2023-01-24 12:58:06 +0100

    Update search page

--- templates/search.php
+++ templates/search.php
@@ -1,3 +1,12 @@
-        <!-- In main -->
-        <label for="Search">Search</label>
-        <input type="search" id="Search"/>
+<!-- In main -->
+
+<!-- Search label and field -->
+<header>
+    <label for="Search">Search</label>
+    <input type="search" id="Search" name="search" placeholder=" Search"/>
+</header>
+
+<!-- Search results -->
+<main>
+    <!--<h2>Hello</h2>-->
+</main>
