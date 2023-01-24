09716d427b9813294591b2977ef69d2d9d704c0d
Francesco Valentini <fv26012001@gmail.com>
2023-01-24 03:07:38 +0100

    Improve date formatting in home

--- templates/home.php
+++ templates/home.php
@@ -10,7 +10,56 @@
         <img src="<?=$user["user_picture_path"]?>" alt="<?=$user["user_id"]?>'s user image"/>
         <div>
             <strong><?=$user["user_name"]?></strong>
-            <p><?=$post["post_time"]?>
+            <p>
+            <?php
+                try {
+                    $time = new DateTime($post["post_time"]);
+                    $ago = time() - $time->format("U");
+                    //convert time in a more human-friendly form
+                    if ($ago < 60) {
+                        if (floor($ago) == 1) {
+                            //exactly one second
+                            echo floor($ago) . " second ago";
+                        } else {
+                            //seconds
+                            echo floor($ago) . " seconds ago";
+                        }
+                    } else if ($ago < 3600) {
+                        if (floor($ago / 60) == 1) {
+                            //exactly one minute
+                            echo floor($ago / 60) . " minute ago";
+                        } else {
+                            //minutes
+                            echo floor($ago / 60) . " minutes ago";
+                        }
+                    } else if ($ago < 86400) {
+                        if (floor($ago / 3600) == 1) {
+                            //exactly one hour
+                            echo floor($ago / 3600) . " hour ago";
+                        } else {
+                            //hours
+                            echo floor($ago / 3600) . " hours ago";
+                        }
+                    } else if ($ago < 604800) {
+                        if (floor($ago / 86400) == 1) {
+                            //exactly one day
+                            echo floor($ago / 86400) . " day ago";
+                        } else {
+                            //days
+                            echo floor($ago / 86400) . " days ago";
+                        }
+                    } else {
+                        if ($time->format("Y") === date("Y")) {
+                            //current year
+                            echo $time->format("F j");
+                        } else {
+                            //earlier date
+                            echo $time->format("F j, Y");
+                        }
+                    }
+                } catch (Exception $e) {
+                }
+            ?>
             <?php if ($post["location"] !== null): ?>
             &nbsp;&bull;&nbsp;&nbsp;<?=$post["location"]?>
             <?php endif; ?>
