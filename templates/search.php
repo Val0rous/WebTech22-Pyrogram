8080b92d64c5b36293f3d4e9fa63d6f4de5b1bea
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2023-01-26 16:58:25 +0100

    Fix some issues

--- templates/search.php
+++ templates/search.php
@@ -15,24 +15,38 @@ if (isset($_GET["search"]) && $_GET["search"] != ""):
     <?php if (!empty($users)): ?>
         <!-- User results box -->
         <section>
-            <?php foreach ($users as $user): ?>
-                <!-- User result -->
-                <a href="user_profile.php?user=<?=$user["user_id"]?>" class="no-underline">
-                    <!-- User profile picture -->
-                    <img src="<?=$user["user_picture_path"]?>" alt="<?=$user["user_id"]?>'s user image">
-                    <!-- User username and name -->
-                    <section>
-                        <p><?=$user["user_id"]?></p>
-                        <p><?=$user["user_name"]?></p>
-                    </section>
-                    <!-- Follow/following button -->
-                    <?php if ($db->findFollowing($_SESSION["user"]["user_id"], $user["user_id"])): ?>
-                        <button type="button" class="search following">Following</button>
-                    <?php elseif ($_SESSION["user"]["user_id"] != $user["user_id"]): ?>
-                        <button type="button" class="search follow">Follow</button>
-                    <?php endif; ?>
-                </a>
-            <?php endforeach; ?>
+            <?php foreach ($users as $user):
+                if ($_SESSION["user"]["user_id"] != $user["user_id"]): ?>
+                    <!-- User result -->
+                    <a href="user_profile.php?user=<?=$user["user_id"]?>" class="no-underline">
+                        <!-- User profile picture -->
+                        <img src="<?=$user["user_picture_path"]?>" alt="<?=$user["user_id"]?>'s user image">
+                        <!-- User username and name -->
+                        <section>
+                            <p><?=$user["user_id"]?></p>
+                            <p><?=$user["user_name"]?></p>
+                        </section>
+                        <!-- Follow/following button -->
+                        <?php if ($db->findFollowing($_SESSION["user"]["user_id"], $user["user_id"])): ?>
+                            <button type="button" class="search following">Following</button>
+                        <?php else: ?>
+                            <button type="button" class="search follow">Follow</button>
+                        <?php endif; ?>
+                    </a>
+                <?php else: ?>
+                    <!-- Found yourself -->
+                    <a href="profile.php" class="no-underline">
+                        <!-- User profile picture -->
+                        <img src="<?=$user["user_picture_path"]?>" alt="<?=$user["user_id"]?>'s user image">
+                        <!-- User username and name -->
+                        <section>
+                            <p><?=$user["user_id"]?></p>
+                            <p><?=$user["user_name"]?></p>
+                        </section>
+                        <p>You</p>
+                    </a>
+                <?php endif;
+            endforeach; ?>
         </section>
     <?php else: ?>
         <p>No results found</p>
