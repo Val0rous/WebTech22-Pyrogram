6a91a9d810704ca34248ed7722328f41542caec8
Federica Bedeschi <federica.bedeschi4@studio.unibo.it>
2023-01-26 16:14:59 +0100

    Update broken user profile style

--- user_profile.php
+++ user_profile.php
@@ -3,7 +3,7 @@ require_once "bootstrap.php";
 
 // Params for the Base Template
 $templateParams["name"] = "user_profile.php";
-$templateParams["css"] = array("user_profile.css", "profile.css");
+$templateParams["css"] = "user_profile.css";
 $templateParams["js"] = array("post.js", "follow_following.js");
 
 $db = new DatabaseHelper();
